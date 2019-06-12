ac998a0c527bd56a95b300c9f8a880cbcf237915
B.yashwanth <36238382+topperbyashwanth@users.noreply.github.com>
2019-06-12 17:56:25 +0530

    Update ReverseString.c

--- ReverseString.c
+++ ReverseString.c
@@ -3,11 +3,11 @@
 int main(void) {
 	int i;
     char ch[100];
-    for(i=0;i<=len(ch);i++)
+    for(i=0;i<=strlen(ch);i++)
     {
     scanf("%c",&ch[i]);
     }
-    for(i=len(ch);i>=0;i--)
+    for(i=strlen(ch);i>=0;i--)
     {
         printf("%c",ch[i]);
     }
