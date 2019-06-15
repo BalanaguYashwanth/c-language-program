3d21b6e8e4d805c86920c5beaf0cc1cae0ffd84f
B.yashwanth <36238382+topperbyashwanth@users.noreply.github.com>
2019-06-15 13:58:49 +0530

    Update ReverseString&RemoveVowels.c

--- ReverseString&RemoveVowels.c
+++ ReverseString&RemoveVowels.c
@@ -2,13 +2,13 @@
 #include<string.h>
 int main()
 {
-    char ch[100]="$35% @ $7";
+    char ch[100];
     int i;
+    scanf("%s",&ch);
     for(i=strlen(ch);i>='\0';i--)
     {
         if(ch[i]!='a'&&ch[i]!='e'&&ch[i]!='i'&&ch[i]!='o'&&ch[i]!='u'&&ch[i]!='s')
         printf("%c",ch[i]);
     }
 }
-
 
