22468c62ac5712eacc3b0b9d784512eca8171c84
B.yashwanth <36238382+topperbyashwanth@users.noreply.github.com>
2019-06-15 14:10:36 +0530

    Update ReverseString&RemoveVowels.c

--- ReverseString&RemoveVowels.c
+++ ReverseString&RemoveVowels.c
@@ -6,10 +6,9 @@ int main()
     int i,z;
     scanf("%d",&z);
     scanf("%s",&ch);
-    for(i=z;i>='\0';i--)
+    for(i=strlen(ch);i>='\0';i--)
     {
         if(ch[i]!='a'&&ch[i]!='e'&&ch[i]!='i'&&ch[i]!='o'&&ch[i]!='u'&&ch[i]!='s')
         printf("%c",ch[i]);
     }
 }
-
