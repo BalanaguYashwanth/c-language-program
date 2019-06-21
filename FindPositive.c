438ba27de8b32ae2ce31202cb83de341c4bdacb8
B.yashwanth <36238382+topperbyashwanth@users.noreply.github.com>
2019-06-21 14:08:34 +0530

    Create FindPositive.c

---
+++ FindPositive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==0)
+    {
+        puts("zero");
+
+    }
+    else if(n<0)
+            {
+                puts("Negative");
+
+            }
+    else{puts("positive");}
+
+}
