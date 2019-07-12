dcd7a99f103034a528f4dbbff373e2365f3ce9a3
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2019-07-12 20:56:03 +0530

    Update Indexsorting.c

--- Indexsorting.c
+++ Indexsorting.c
@@ -11,6 +11,9 @@ int main()
    for(i=0;i<n;i++)
     {
         if(a[i]==i)
+        if(i==0)
+            printf("%d",a[i]);
+        else if(i!=0)
             printf(" %d",a[i]);
         else
         count++;
@@ -20,4 +23,3 @@ int main()
         printf("-1");
 
 }
-
