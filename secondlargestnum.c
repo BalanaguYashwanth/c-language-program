eff2669bd4ce83d3c7f49ce319546361723017ef
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2019-08-21 17:44:41 +0530

    Create secondlargestnum.c

---
+++ secondlargestnum.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a[20],i,max=0,sdmax=0,temp=0;
+    for(i=0;i<=5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    max=a[0];
+    sdmax=a[1];
+    for(i=0;i<=5;i++)
+    {
+      if(max<a[i])
+      {
+          temp=max;
+          max=a[i];
+
+      }
+      else
+        if(max>a[i] && sdmax<a[i])
+      {
+           sdmax=a[i];
+           temp=sdmax;
+      }
+
+    }
+
+
+printf("the value is %d",sdmax);
+
+}
