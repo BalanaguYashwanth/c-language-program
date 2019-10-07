b9e8c1b066cb3ab6b23c489b55d2acbd9add1be5
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2019-10-07 21:27:11 +0530

    Update MissingLetter.c

--- MissingLetter.c
+++ MissingLetter.c
@@ -1,28 +1,21 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-    char ch[100],ch1[100],count=0;
+    char a[10],b[10];
     int i,j;
-    printf("enter the first string");
-    scanf("%s",ch);
-    printf("enter the second string");
-    scanf("%s",ch1);
-    for(i=0;i<=strlen(ch);i++)
+    printf("enter the 1st string");
+    scanf("%s",a);
+    printf("enter the 2nd string");
+    scanf("%s",b);
+    for(i=0;a[i]!='\0';i++)
     {
-        count=0;
-        for(j=0;j<=strlen(ch1);j++)
+        if(a[i]==b[i])
         {
-            if(ch[i]!=ch1[j])
-            {
-                count++;
-
-            if(count==strlen(ch))
-            printf("\nThe missed character is  %c",ch[i]);
-            }
-
+            continue;
         }
-
+        else
+        {
+            printf("the element is %c",a[i]);
+        }
     }
-
 }
