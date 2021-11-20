63a2f6e0e57b3294278cb3029980d2472da600cf
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-11-20 23:29:44 +0530

    Add files via upload

--- C Language-2021/addition.c
+++ C Language-2021/addition.c
@@ -2,5 +2,13 @@
 void main()
 {
     int i=2,b=10;
-    printf("%d",i*b);
+    printf("%d",9%2);
+    if(!(-2))
+    {
+        printf("yes");
+    }
+    if(printf("_"))
+    {
+        printf("yes");
+    }
 }
